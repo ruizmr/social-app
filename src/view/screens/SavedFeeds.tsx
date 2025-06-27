a467af8b9e3e1fcd6463005b7b3174d866b9ccb4
Samuel Newman <mozzius@protonmail.com>
2025-06-27 11:26:59 +0300

    adjust styles, alf stuff

--- src/view/screens/SavedFeeds.tsx
+++ src/view/screens/SavedFeeds.tsx
@@ -393,8 +393,8 @@ function FollowingFeedCard() {
         a.align_center,
         a.flex_1,
         {
-          paddingHorizontal: 18,
-          paddingVertical: 20,
+          paddingHorizontal: 16,
+          paddingVertical: 16,
         },
       ]}>
       <View
